b290d884e4572eb9a5bc7324d9e4d32c9bc693bd
kingregi1 <59853870+kingregi1@users.noreply.github.com>
2020-02-06 09:36:03 -1000

    Initial Commit

---
+++ HachwichName/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HachwichName
+//
+//  Created by Reginald T King on 2/6/20.
+//  Copyright © 2020 Reginald T King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
